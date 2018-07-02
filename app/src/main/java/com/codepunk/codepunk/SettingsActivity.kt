024506c93f752f8ed178c56b9ccfa305e642cbfb
Scott Slater <slaterama@gmail.com>
2018-07-02 19:25:00 -0400

    SettingsActivity saves/remembers developer status

--- app/src/main/java/com/codepunk/codepunk/SettingsActivity.kt
+++ app/src/main/java/com/codepunk/codepunk/SettingsActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.Preference
 import android.support.v7.preference.Preference.OnPreferenceClickListener
 import android.support.v7.preference.PreferenceFragmentCompat
-import android.util.Log
 import android.widget.Toast
 
 class SettingsActivity : AppCompatActivity() {
@@ -21,33 +20,44 @@ class SettingsActivity : AppCompatActivity() {
             private const val DEVELOPER_CLICK_COUNT_TOAST: Int = 4
             private val KEY_DEVELOPER_CLICK_COUNT: String =
                     SettingsFragment::class.java.name + ".DEVELOPER_CLICK_COUNT"
+
+            private const val PREF_KEY_DEVELOPER_OPTIONS = "key_developer_options"
             private const val PREF_KEY_VERSION = "key_version"
+
+            private const val PREF_KEY_DEVELOPER = "key_developer"
         }
 
         //endregion Nested classes
 
         //region Fields
 
-        private var _developer = false
-        private var developer
-            get() = _developer
-            set(value) {
-                _developer = value
-                createPreferences()
-            }
-
-        private var versionPreference: Preference? = null
+        private var developer = false
             set(value) {
                 field = value
-                value?.summary = getString(R.string.summary_version, versionName)
-                value?.onPreferenceClickListener = this
+                preferenceManager.sharedPreferences
+                        .edit()
+                        .putBoolean(PREF_KEY_DEVELOPER, value)
+                        .apply()
+                if (value) {
+                    preferenceScreen.addPreference(developerOptionsPreference)
+                } else {
+                    preferenceScreen.removePreference(developerOptionsPreference)
+                }
             }
+
+        private var developerClickCount: Int = 0
+
+        private val developerOptionsPreference: Preference by lazy {
+            findPreference(PREF_KEY_DEVELOPER_OPTIONS)
+        }
 
         private val versionName: String? by lazy {
             activity?.packageManager?.getPackageInfo(activity?.packageName, 0)?.versionName
         }
 
-        private var developerClickCount: Int = 0
+        private val versionPreference: Preference by lazy {
+            findPreference(PREF_KEY_VERSION)
+        }
 
         //endregion Fields
 
@@ -68,7 +78,10 @@ class SettingsActivity : AppCompatActivity() {
         //region Inherited methods
 
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            createPreferences(rootKey)
+            setPreferencesFromResource(R.xml.preferences_main, rootKey)
+            versionPreference.summary = getString(R.string.summary_version, versionName)
+            versionPreference.onPreferenceClickListener = this
+            developer = preferenceManager.sharedPreferences.getBoolean(PREF_KEY_DEVELOPER, false)
         }
 
         //endregion Inherited methods
@@ -79,7 +92,7 @@ class SettingsActivity : AppCompatActivity() {
             developerClickCount++
             var toastText: CharSequence? = null
             when {
-                developerClickCount > DEVELOPER_CLICK_COUNT_UNLOCK -> {
+                developer -> {
                     toastText = getString(R.string.already_developer)
                 }
                 developerClickCount == DEVELOPER_CLICK_COUNT_UNLOCK -> {
@@ -103,18 +116,6 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         //endregion Implemented methods
-
-        //region Private methods
-
-        private fun createPreferences(rootKey: String? = null) {
-            setPreferencesFromResource(
-                    if (developer) R.xml.preferences_main_developer else R.xml.preferences_main,
-                    rootKey)
-            versionPreference = findPreference(PREF_KEY_VERSION)
-            Log.d("Test", versionPreference?.toString() ?: "versionPreference is null")
-        }
-
-        //endregion Private methods
     }
 
     //endregion Nested classes
